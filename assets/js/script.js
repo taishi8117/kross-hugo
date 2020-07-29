8bfaf7b919e1efeddb6abb92efe3a5b2b977fee0
Taishi Nojima <taishi.nojima@aya.yale.edu>
2020-07-30 01:16:01 +0900

    Mostly index layout changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,13 +7,13 @@ $(window).on('load', function () {
 	'use strict';
 
 	// Sticky Menu
-	//$(window).scroll(function () {
-	//	if ($('.navigation').offset().top > 100) {
-	//		$('.navigation').addClass('nav-bg');
-	//	} else {
-	//		$('.navigation').removeClass('nav-bg');
-	//	}
-	//});
+	$(window).scroll(function () {
+		if ($('.navigation').offset().top > 100) {
+			$('.navigation').addClass('nav-bg');
+		} else {
+			$('.navigation').removeClass('nav-bg');
+		}
+	});
 
 	// Background-images
 	$('[data-background]').each(function () {
